6b12c5a9fe7e3c3a335a2bb82492e7d2e5058ed4
Kingsley Anyichie <111355135+Kingston7777777@users.noreply.github.com>
2022-10-03 00:15:48 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onresize = screen;//when you re-size your window
-window.onload = screen;//when your re-sizing your loading
-
-function screen(){//Function to check width of your browser window
-    myWidth = window.innerWidth;//window innerwidth
-    let move = document.getElementById("size");
-    move.style.fontSize = "25px";
-    move.innerHTML = "Width : " + myWidth + "px";
-}
